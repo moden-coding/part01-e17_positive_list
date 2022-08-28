4e72d12ccbf731fb899d146153784b7c7bd712df
modengann <99417989+modengann@users.noreply.github.com>
2022-08-28 13:10:20 -0400

    init

---
+++ src/positive_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def is_positive(n):
+    return n > 0
+
+def positive_list(L):
+    return list(filter(is_positive,L))
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
